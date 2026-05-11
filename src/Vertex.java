9a88da74d0551ab0ce553b8f43f6ceb7fc5f31e1
Lunie_1 <6akhr1yar@gmail.com>
2026-05-11 21:20:39 +0500

    first check

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,2 +1,21 @@
 public class Vertex {
+
+    private final int id;
+
+    public Vertex(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean equals(Vertex other) {
+        return this.id == other.id;
+    }
+
+    @Override
+    public String toString() {
+        return "Vertex#" + id;
+    }
 }
